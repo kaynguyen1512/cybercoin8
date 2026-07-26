362da7fd226d9d6a0d8a7ecb367b45c5ed807909
anymailfinder03 <anymailfinder03@gmail.com>
2026-07-26 22:21:33 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,6 @@ export default function App() {
         </div>
         <About />
         <CrewDatabase />
-        <Gallery />
         <Tokenomics />
         <CyberpsychoMeter />
         <Roadmap />
